568ac57735d9cf98504263f09ed0c0029ce26690
Visa Pollari <visa@smartly.io>
2018-08-22 16:48:07 +0300

    Fix colorkey loading

--- engine/graphicalAssetHandler.py
+++ engine/graphicalAssetHandler.py
@@ -14,7 +14,7 @@ class GraphicalAssetHandler(dict):
     self.load("PORTRAIT", colorkey_pos=(0, 0))
     self.load("BGR")
 
-  def load(self, end_path, colorkey_pos=(-1, -1), flags=[]):
+  def load(self, end_path, colorkey_pos=False, flags=[]):
     self[end_path] = dict()
     path_to_folder = os.path.join(self.asset_path, end_path)
     for root, dirs, files in os.walk(path_to_folder):
@@ -22,8 +22,8 @@ class GraphicalAssetHandler(dict):
         if file.endswith(".png"):
           file_path = os.path.join(path_to_folder, file)
           base_name = file.split(os.path.sep)[-1].split(".")[0]
-
           img = pygame.image.load(file_path).convert()
-          img.set_colorkey(img.get_at(colorkey_pos), *flags)
+          if(colorkey_pos):
+            img.set_colorkey(img.get_at(colorkey_pos), *flags)
 
           self[end_path][base_name] = img

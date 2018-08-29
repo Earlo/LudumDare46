c5ffdb16e741bc06281885c36af794b285615918
Visa Pollari <visa@smartly.io>
2018-08-29 19:06:14 +0300

    Clean up entity

--- engine/graphics/entity.py
+++ engine/graphics/entity.py
@@ -7,15 +7,19 @@ class Entity(Sprite):
   def __init__(self, GAME, pos, time=pygame.time.get_ticks()):
     super().__init__(GAME, "sprites")
     self.topleft = pos
+    self.float_pos = pos
     self.created_at = time
-    self.updated_at = self.created_at
+    # self.updated_at = self.created_at
     self.timeInterval = 0
 
+    self.velocity = (0.001, 0.001)
+
   def tick(self, t):
-    self.timeInterval = t - self.updated_at
-    self.updated_at = t
-    # TODO remove
-    self.move_ip(1, 1)
+    self.timeInterval = t
+
+    movement = [x * self.timeInterval for x in self.velocity]
+    self.float_pos = tuple(map(sum, zip(self.float_pos, movement)))
+    self.topleft = self.float_pos
 
     self.draw()
 

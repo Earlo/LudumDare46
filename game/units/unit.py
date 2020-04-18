ab7e9f8e3b863907b503d621a605fc94169e2253
Visa Pollari <visapollari@Visas-MacBook-Pro.local>
2020-04-18 19:43:05 +0300

    WIP

--- game/units/unit.py
+++ game/units/unit.py
@@ -7,12 +7,16 @@ from ..unit_logic.move_to import MoveTo
 
 
 class Unit(Entity):
-    sprite = ["frog"]
+    idleframes = ["olive0"]
+    walkingframes = ["olive0", "olive1", "olive2"]
+    frames = idleframes
+    animationSpeed = 10.0
     graphic_layer = 1
 
     def __init__(self, GAME, pos):
         super().__init__(pos)
         self.direction = pi
+        self.moving = False
         self.speed = 0.2
         self.task = None
 
@@ -25,10 +29,11 @@ class Unit(Entity):
                 self.float_pos[0] + random() * 100 - random() * 100,
                 self.float_pos[1] + random() * 100 - random() * 100,
             )
-            print(target)
             self.task = MoveTo(self, target)
-            # when idle, select a nearby and wander there
-            pass
+        if self.moving:
+            self.frames = self.walkingframes
+        else:
+            self.frames = self.idleframes
         super().tick(t)
 
     def assign(self, task, complete_task):
@@ -37,4 +42,7 @@ class Unit(Entity):
 
     @property
     def velocity(self):
-        return (cos(self.direction) * self.speed, sin(self.direction) * self.speed)
+        if self.moving:
+            return (cos(self.direction) * self.speed, sin(self.direction) * self.speed)
+        else:
+            return (0, 0)

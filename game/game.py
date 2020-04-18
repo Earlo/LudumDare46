9ef783f56f58c02439de269669a30d84fdb5675b
Kumkvatti <hansen.feng50@gmail.com>
2020-04-18 17:27:51 +0300

    Implement plant entity

--- game/game.py
+++ game/game.py
@@ -1,6 +1,7 @@
 from engine.metaGame import MetaGame
 from .level import ExampleLevel
 from .unit import Unit
+from .plant import Plant
 
 from .ui.screens import testGui, noGui
 from engine.constants import SWIDTH, SHEIGTH
@@ -20,9 +21,9 @@ class Game(MetaGame):
 
     def START(self):
         self.add_cameraport("GAME", SWIDTH, SHEIGTH)
-
         self.entities.append(Unit(self, (500.0, 500.0), (-0.01, -0.01)))
         self.entities.append(Unit(self, (0.0, 200.0), (0.01, 0.01)))
+        self.entities.append(Plant(self, (0.0, 0.0), 0))
         self.level = ExampleLevel(self)
 
         self.load_gui(noGui(self))

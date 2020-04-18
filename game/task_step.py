3c4014051681458ff932c59b08f7d871861d72bb
Ilmari Tarpila <leo.tarpila@aalto.fi>
2020-04-18 16:28:24 +0300

    Task manager, tasks and task steps

---
+++ game/task_step.py
@@ -0,0 +1,6 @@
+class TaskStep():
+
+    def __init__(self, title, substeps):
+        self.title = title
+        self.substeps = substeps
+    
---
+++ game/task_steps.py
@@ -0,0 +1,4 @@
+from task_step import TaskStep
+
+FARM = TaskStep("Do farming", [])
+GO_HOME = TaskStep("Go home", [])
